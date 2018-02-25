7c0677b456cb7710ec33f153ca270a18a12eb993
garnich <garnich@tut.by>
2018-02-26 01:37:37 +0300

    second task

--- src/index.js
+++ src/index.js
@@ -4,4 +4,36 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-};
+
+
+  var i;
+  var j ;
+  var k;
+  var d;
+  var z;
+  var w;
+  var t = 0 ;
+  var m =[];
+  var el =[];
+
+   for (i=0; i <= preferences.length-1; i++){
+      j = preferences[i];
+      k = preferences[j-1];
+
+      if( k == j){i++}
+      else{
+      d = preferences[k-1];
+      z = preferences[d-1];
+      if(z==j){
+        m.push(j, k, d);
+
+    }}
+  }
+
+        el = Array.from(new Set(m));
+
+    w = el.length/3;
+
+
+        return w;
+}
