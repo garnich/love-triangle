857cb90f46b8957559be503a58058cdbf9e44af2
garnich <garnich@tut.by>
2018-09-10 22:48:44 +0300

    second_attempt

--- src/index.js
+++ src/index.js
@@ -6,34 +6,20 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
 
 
-  var i;
-  var j ;
-  var k;
-  var d;
-  var z;
-  var w;
-  var t = 0 ;
+  var i,j,k,d,z;
   var m =[];
-  var el =[];
 
-   for (i=0; i <= preferences.length-1; i++){
+   for (i=0; i <= preferences.length-1; i++)
+   {
       j = preferences[i];
       k = preferences[j-1];
 
-      if( k == j){i++}
-      else{
-      d = preferences[k-1];
-      z = preferences[d-1];
-      if(z==j){
-        m.push(j, k, d);
-
-    }}
+        if( k == j){i++}
+          else
+          { d = preferences[k-1];
+            z = preferences[d-1];
+              if(z==j){m.push(j, k, d);}
+    }
   }
-
-        el = Array.from(new Set(m));
-
-    w = el.length/3;
-
-
-        return w;
+  return (Array.from(new Set(m))).length/3;
 }
